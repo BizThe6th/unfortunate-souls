99b62f5804ada40043efc1a51eb3b9a6a032dfd2
BuildTools <unconfigured@null.spigotmc.org>
2024-02-13 18:27:19 -0500

    Changed dialog, added a batch file that runs starbound.

---
+++ runstarbound.bat
@@ -0,0 +1,9 @@
+@echo off
+rem "rem" means comment, lmao
+rem "@echo off" makes it so that the script doesn't print "rem" or "comment" statements
+
+setlocal EnableDelayedExpansion
+echo Starting Starbound...
+rem the following runs starbound through steam
+start steam://rungameid/211820
+echo Starbound has started.
